02ba28366283f9df5ffa5e3cbab2bda0a935e189
Carlos Emilio <cephoenix@gmail.com>
2021-02-09 10:05:47 -0300

    Reestruturando a aplicação

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("ASDF");
+var app = require('./config/server');
+
+var rotaHome = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaFormInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
+
+app.listen(3000, () => {
+	console.log("Servidor iniciado com sucesso!");
+});
---
+++ mod_teste.js
@@ -0,0 +1,6 @@
+var msg = "Modulo => string";
+
+module.exports =
+{
+	msg
+}
